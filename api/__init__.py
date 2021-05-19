b3477cddd4c4ae297bf545b4b19406757e574b4d
Bennet Rupnow <bennet.rupnow@gym-meiendorf.de>
2021-05-19 19:42:19 +0200

    add basic flask

--- api/__init__.py
+++ api/__init__.py
@@ -1,18 +1,19 @@
+import markdown
+import os
+
 from flask import Flask
 from flask_restful import Api
-import markdown;
-import os;
-
-
-from api.devices import Devices;
+from api.devices import Devices
+from waitress import serve
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Devices, "/devices")
-
 @app.route("/")
 def index():
-    with open(os.path.dirname(app.root_path) + '/README.md') as markdown_file:
-        content = markdown_file.read();
-        return markdown.markdown(content);
+    with open(os.getcwd() + '\README.md') as markdown_file:
+        content = markdown_file.read()
+        return markdown.markdown(content)
+
+
+serve(app, port=5000)
