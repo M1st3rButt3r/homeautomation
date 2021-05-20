b1602620a8bb5e14ea65eed8fe778e1b9de0267c
Bennet Rupnow <bennet.rupnow@gym-meiendorf.de>
2021-05-20 17:09:15 +0200

    add basic api

--- api/__init__.py
+++ api/__init__.py
@@ -2,12 +2,19 @@ import markdown
 import os
 
 from flask import Flask
+import flask.scaffold
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
 from flask_restful import Api
 from api.devices import Devices
+from api.device import Device
 from waitress import serve
 
 app = Flask(__name__)
 api = Api(app)
+
+api.add_resource(Devices, "/devices")
+api.add_resource(Device, "/device/<string:identifier>")
+
 
 @app.route("/")
 def index():
--- api/device.py
+++ api/device.py
@@ -1,17 +1,18 @@
 from flask_restful import Resource, abort, reqparse
 
 
-class Devices(Resource):
-    def get(self):
+class Device(Resource):
+    def get(self, identifier):
         return {"message": "not implemented"}
 
-    def post(self):
+    def post(self, identifier):
         parser = reqparse.RequestParser()
 
-        parser.add_argument('name', required=True)
-        parser.add_argument('type', required=True)
-        parser.add_argument('gateway', required=True)
+        parser.add_argument('action', required=True)
 
         args = parser.parse_args()
 
+        return {"message": "not implemented"}
+
+    def delete(self, identifier):
         return {"message": "not implemented"}
